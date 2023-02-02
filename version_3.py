873327a950133f61ad1381e69d59fc33432bbe1f
Alexander Mangazeev <alex3287@bk.ru>
2023-02-02 16:12:25 +0300

    version 3

---
+++ version_3.py
@@ -0,0 +1,22 @@
+login = input('Введите логин -> ')
+print(login + ',', 'добро пожаловать в игру')
+
+#task3
+price_tt = int(input('Введите стоимость одного тт -> '))
+price_st = int(input('Введите стоимость одного cт -> '))
+price_lt = int(input('Введите стоимость одного лт -> '))
+
+suma = price_tt * 4 + price_st * 2 + price_lt * 3
+print(suma)
+
+#task4
+price_k = int(input('->'))
+price_p = int(input('->'))
+suma = price_k*80 + price_p*65
+print(suma)
+
+#task 5
+money = int(input('-> '))
+price_tank = int(input('-> '))
+print(money // price_tank)
+
